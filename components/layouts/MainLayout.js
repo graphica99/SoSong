84c33a37e57fc0e9ecf13eb562184fe677569672
mends albert <mendsalbert@gmail.com>
2021-06-26 15:31:03 +0100

    position fixed problem music player card

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -25,6 +25,14 @@ export default class MainLayout extends Component {
               href="https://fonts.googleapis.com/css2?family=Allura&family=Mr+Dafoe&display=swap"
               rel="stylesheet"
             />
+            <meta
+              content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+              name="viewport"
+            />
+            {/* <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            ></meta> */}
           </Head>
           <SideBar />
           {this.props.children}
